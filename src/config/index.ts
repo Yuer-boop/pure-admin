826654b975d98261d6de0b48fb307e2e42da9b8c
Yuer <img95954@gmail.com>
2025-12-02 16:33:08 +0800

    fix: 1

--- src/config/index.ts
+++ src/config/index.ts
@@ -31,7 +31,7 @@ export const getPlatformConfig = async (app: App): Promise<undefined> => {
   app.config.globalProperties.$config = getConfig();
   return axios({
     method: "get",
-    url: `/platform-config.json`
+    url: `pure-admin/platform-config.json`
   })
     .then(({ data: config }) => {
       let $config = app.config.globalProperties.$config;
