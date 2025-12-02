346a162f3e66ada3e80927184ed5aa6956ecff90
Yuer <img95954@gmail.com>
2025-12-02 16:30:51 +0800

    fix: 1

--- src/config/index.ts
+++ src/config/index.ts
@@ -28,26 +28,25 @@ const getConfig = (key?: string): PlatformConfigs => {
 
 /** 获取项目动态全局配置 */
 export const getPlatformConfig = async (app: App): Promise<undefined> => {
-  // app.config.globalProperties.$config = getConfig();
-  // return axios({
-  //   method: "get",
-  //   url: `${VITE_PUBLIC_PATH}platform-config.json`
-  // })
-  //   .then(({ data: config }) => {
-  //     let $config = app.config.globalProperties.$config;
-  //     // 自动注入系统配置
-  //     if (app && $config && typeof config === "object") {
-  //       $config = Object.assign($config, config);
-  //       app.config.globalProperties.$config = $config;
-  //       // 设置全局配置
-  //       setConfig($config);
-  //     }
-  //     return $config;
-  //   })
-  //   .catch(() => {
-  //     throw "请在public文件夹下添加platform-config.json配置文件";
-  //   });
-  return Promise.resolve(undefined);
+  app.config.globalProperties.$config = getConfig();
+  return axios({
+    method: "get",
+    url: `/platform-config.json`
+  })
+    .then(({ data: config }) => {
+      let $config = app.config.globalProperties.$config;
+      // 自动注入系统配置
+      if (app && $config && typeof config === "object") {
+        $config = Object.assign($config, config);
+        app.config.globalProperties.$config = $config;
+        // 设置全局配置
+        setConfig($config);
+      }
+      return $config;
+    })
+    .catch(() => {
+      throw "请在public文件夹下添加platform-config.json配置文件";
+    });
 };
 
 /** 本地响应式存储的命名空间 */
