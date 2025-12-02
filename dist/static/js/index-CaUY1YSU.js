feb923cbfab72c89e0641f2c94a045a09648144d
Yuer <img95954@gmail.com>
2025-12-02 15:21:23 +0800

    fix: 1

--- dist/static/js/index-CaUY1YSU.js
+++
@@ -1,9 +0,0 @@
-import { d as e, c as n, o as t } from "./index-B2fHy2Kp.js";
-const s = e({
-  name: "",
-  __name: "index",
-  setup(o) {
-    return (a, c) => (t(), n("div", null, "test"));
-  }
-});
-export { s as default };
